323d971fb9f26202a61bb727ed8dec08471f1b45
ihebchebbi1998tn <iheb.chebbi@lcieducation.net>
2025-01-25 13:18:01 +0100

    adadad

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -1,11 +1,12 @@
 import * as React from "react"
+
 import type {
   ToastActionElement,
   ToastProps,
 } from "@/components/ui/toast"
 
 const TOAST_LIMIT = 1
-const TOAST_REMOVE_DELAY = 1000000
+const TOAST_REMOVE_DELAY = 3000 // Set to 3 seconds
 
 type ToasterToast = ToastProps & {
   id: string
@@ -70,7 +71,7 @@ const addToRemoveQueue = (toastId: string) => {
   toastTimeouts.set(toastId, timeout)
 }
 
-const reducer = (state: State, action: Action): State => {
+export const reducer = (state: State, action: Action): State => {
   switch (action.type) {
     case "ADD_TOAST":
       return {
@@ -89,6 +90,8 @@ const reducer = (state: State, action: Action): State => {
     case "DISMISS_TOAST": {
       const { toastId } = action
 
+      // ! Side effects ! - This could be extracted into a dismissToast() action,
+      // but I'll keep it here for simplicity
       if (toastId) {
         addToRemoveQueue(toastId)
       } else {
@@ -157,6 +160,9 @@ function toast({ ...props }: Toast) {
       },
     },
   })
+
+  // Automatically dismiss toast after TOAST_REMOVE_DELAY
+  setTimeout(dismiss, TOAST_REMOVE_DELAY)
 
   return {
     id: id,
